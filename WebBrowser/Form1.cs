6635cda4afa088d493da1eb5bfbd33b65d862eda
Noah Sterling <noah.sterling@brightstraining.com>
2023-12-13 22:18:12 +0100

    Add project files.

---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+	public partial class Index : Form
+	{
+		public Index()
+		{
+			InitializeComponent();
+		}
+
+		private void toolStripComboBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.Enter)
+			{
+				webBrowser1.Navigate(toolStripComboBox1.Text);
+			}
+		}
+
+		private void btnBack_Click(object sender, EventArgs e)
+		{
+			webBrowser1.GoBack();
+		}
+
+		private void btnForward_Click(object sender, EventArgs e)
+		{
+			webBrowser1.GoForward();
+		}
+
+		private void Go_Click(object sender, EventArgs e)
+		{
+			webBrowser1.Navigate(toolStripComboBox1.Text);
+		}
+
+		
+	}
+}
